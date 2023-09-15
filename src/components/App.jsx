f5a4e58ef74a1246c9a413da81a04a6ce8008467
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-09-15 22:04:21 +0200

    step-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,43 +3,66 @@ import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [],
-    name: ''
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: ''
   };
 
   handleInputChange = event => {
-    this.setState({ name: event.currentTarget.value });
+    const { name, value } = event.currentTarget;
+    this.setState({
+      [name]: value
+    });
   };
+
+  changeFilter = (event) => {
+    this.setState({ filter: event.currentTarget.value })
+  }
+
+  findContact = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLocaleLowerCase();
+    return contacts.filter(contact => contact.name.toLocaleLowerCase().includes(normalizedFilter),)
+  }
 
   handleInputSubmit = event => {
     event.preventDefault();
-    const { name } = this.state;
+    const { name, number } = this.state;
 
-    if (name.trim() === '') {
+    if (name.trim() === '' || number.trim() === '') {
       alert('Please enter a valid name.');
       return;
     }
 
     const newContact = {
       id: nanoid(),
-      name: name
+      name: name,
+      number: number
     };
 
     this.setState((prevState) => ({
       contacts: [...prevState.contacts, newContact],
-      name: ''
+      name: '',
+      number: ''
     }));
   };
 
   render() {
-    const { contacts, name } = this.state;
+    const findContact = this.findContact()
+    const { name, number, filter } = this.state;
 
     return (
       <div>
         <h1>Phonebook</h1>
         <form onSubmit={this.handleInputSubmit}>
           <label>
-            Name
+            Name 
             <input
               type="text"
               name="name"
@@ -50,12 +73,32 @@ export class App extends Component {
               required
             />
           </label>
+          <label >
+            Number
+            <input
+              type="tel"
+              value={number}
+              onChange={this.handleInputChange}
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
+        <label>
+          Find contacts by Name
+          <input
+            type="text"
+            value={filter}
+            onChange={this.changeFilter}
+          />
+        </label>
         <ul>
-          {contacts.map((contact) => (
-            <li key={contact.id}>{contact.name}</li>
+          {findContact.map((contact) => (
+            <li key={contact.id}>{contact.name} : {contact.number}</li>
           ))}
         </ul>
       </div>
